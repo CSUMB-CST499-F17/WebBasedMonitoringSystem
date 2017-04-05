9afdf34879d71df86b42235137b75ec48f96e18c
brandanlockwood <blockwood@csumb.edu>
2017-04-05 13:42:32 -0700

    get suite name plus if running local host find suite
    Summary: fix merge conflicts
    
    Test Plan: Test getLocalHost
    
    Reviewers: kp1zano, stephenenegron
    
    Reviewed By: kp1zano, stephenenegron
    
    Differential Revision: https://web-based-weather-workflow.phacility.com/D10

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_socketio import emit,send
 import json
 from flask import jsonify
 from requests.auth import HTTPDigestAuth
+import socket
 app = flask.Flask(__name__)
 
 
@@ -20,31 +21,79 @@ def hello():
 def on_connect():
  print "%s USER CONNECTED " %  flask.request.sid
 
+
 @socketio.on('data')
 def message(message):
  app.logger.warning('A warning occurred (%d apples)', 42)
  app.logger.error('An error occurred')
  app.logger.info('Info')
-
+# print message
+# print message["hostName"]
  link = "http://"+ str( message["hostName"]) + ":" + str(message["portNumber"]) + '/state/get_state_summary'
  #Verify not necessary: TODO: Verify = FALSE should not skip authentication.
  r=requests.get(link,auth=HTTPDigestAuth('anon','the quick brown fox'))
  data = r.json()
  emit ('summary_info',data)
-  
+
+@socketio.on('localData')
+def local(message):
+ app.logger.warning('A warning occurred (%d apples)', 42)
+ app.logger.error('An error occurred')
+ app.logger.info('Info')
+# print message
+# print message["hostName"]
+ link = "http://"+ str(socket.gethostname()) + ":" + str(message["portNumber"]) + '/state/get_state_summary'
+ #Verify not necessary: TODO: Verify = FALSE should not skip authentication.
+ r=requests.get(link,auth=HTTPDigestAuth('anon','the quick brown fox'))
+ data = r.json()
+ emit ('summary_info',data)
 @socketio.on('disconnect')
 def on_disconnect():
  print "USER DISCONNECTED"
+
+
+@socketio.on('getName')
+def name(message):
+ # print message
+# print message["hostName"]
+ portNumber=43000
+ r=["Error in request"]
+ print r[0]
+ while portNumber<43101:
+ 	link = "http://"+ str(socket.gethostname()) + ":"+str(portNumber) + '/id/identify'
+ #Verify not necessary: TODO: Verify = FALSE should not skip authentication.
+        portNumber=portNumber+1
+ 	try:
+		 r=requests.get(link,auth=HTTPDigestAuth('anon','the quick brown fox'))
+		 r=r.json()
+		 print json.dumps(r,indent=2)
+                 print r["name"]
+		 print message
+                 print portNumber
+                 if message["suiteName"] in r["name"]:
+                        portNumber=portNumber-1
+			break
+ 	except requests.ConnectionError, e:
+		print e
+        	r=["Error in request"]
+
+
+ #print json.dumps(data,indent=2)
+ emit ('name',{"name":r["name"],"portNumber":portNumber})
+
+
 
 @socketio.on('stop_suite')
 def stop_suite(message):
     stop_link = "https://"+ str( message["hostName"]) + ":" + str(message["portNumber"]) + '/state/get_state_summary'
     stopped_task=requests.get(link,auth=HTTPDigestAuth('anon','the quick brown fox'), verify=False)
 
-    
+
+
 if __name__ == '__main__':# __name__!
 
  socketio.run(
  app,
+ host=socket.gethostname(),
  debug=True
  )
