3845a078cd77f403c27edbbbfc6b751a8518a05d
kp1zano <kpizano@csumb.edu>
2017-04-19 12:41:26 -0700

    Fixed Buttons, Grabbed Tasks on JS
    Summary: Paused Fixed, grabbed cycling tasks and the tasks running for each one.
    
    Test Plan: Still need to display the cycling tasks correctly on html.
    
    Reviewers: stephenenegron, blockwood
    
    Reviewed By: stephenenegron, blockwood
    
    Differential Revision: https://web-based-weather-workflow.phacility.com/D13

--- static/script.js
+++ static/script.js
@@ -2,7 +2,7 @@
 window.onload = function(){
     document.getElementById("information").style.visibility = "hidden";
     document.getElementById("table").style.visibility = "hidden";
-    document.getElementById("updated").style.visibility = "hidden";
+    //document.getElementById("updated").style.visibility = "hidden";
 }
 
 
@@ -12,6 +12,7 @@ var suiteName="";
 var hostName="";
 var suite_information;
 var nodePosition;
+var paused_suite = false;
 
 
 
@@ -30,27 +31,27 @@ function getData(){
     }, 3000);
 }
 
-/**
- * Play suite after pausing.
- *
- */
-
 function playSuite(){
     getData();
 }
 
 function pauseSuite(){
     clearInterval(suite_information);
+    paused_suite = true;
 }
 
 function stopSuite(){
-    clearInterval(suite_information);
-    hostName=$('#hostName').val();
-    portNumber=$('#portNumber').val();
-        socket.emit('stop_suite', {
-            'hostName':hostName,
-            'portNumber':portNumber
-        });
+
+    if(paused_suite){
+        
+        clearInterval(suite_information);
+        hostName=$('#hostName').val();
+        portNumber=$('#portNumber').val();
+            socket.emit('stop_suite', {
+                'hostName':hostName,
+                'portNumber':portNumber
+            });
+    }
 }
 
 function getLastNodePosition(){
@@ -79,22 +80,16 @@ function getLocalData(){
       }, 3000);
     }
   })
-
-
-
 }
-
-
-last_updated="";
 
 socket.on('summary_info',function(data){
     var nodes=data[1];
     var getAllKeys = Object.keys(nodes);
     var getSingleKey= Object.keys(nodes)[0];
-    //console.log(data[getAllKeys[5]]);
     var stateTotals=data[0]["state totals"];
     var currentStates = data[0]["states"];
     var numOfStates = data[0]["states"].length;
+
     //If ran locally data[3] will not be defined
     if(data[3]!=undefined)
     {
@@ -103,6 +98,7 @@ socket.on('summary_info',function(data){
   //set suiteName when rest of the data comin in
   $("#nameOfSuite").text(suiteName);
     //console.log(suiteName);
+
 
     //Respectively: white - 0, pink - 1, red - 2, khaki - 3, gold - 4, lime - 5, green - 6, deep-sky-blue - 7, blue - 8, light-gray - 9, gray - 10, black - 11
     var colors = {"white":"#FFFFFF", "pink":"#FF1493", "red":"#FF0000", "khaki":"#F0E68C", "gold":"#FFD700", "lime":"#00FF00", "green":"#008000", "deep-sky-blue":"#00BFFF", "blue":"#0000FF", "light-gray":"#D3D3D3", "gray":"#808080", "black":"#000000"};
@@ -201,58 +197,29 @@ socket.on('summary_info',function(data){
     }
         //guaranteed by our suite
 		//TODO:just create these d3 objs dynamically
+
 			d3.select("body").select("#nodeOne").text(nodes[getAllKeys[0]]["name"] +" "+nodes[getAllKeys[0]]["state"]+ " "+nodes[getAllKeys[0]]["label"]);
 		  d3.select("body").select("#nodeTwo").text(nodes[getAllKeys[1]]["name"]+" "+nodes[getAllKeys[1]]["state"]+ " "+nodes[getAllKeys[1]]["label"]);
 			d3.select("body").select("#nodeThree").text(nodes[getAllKeys[2]]["name"]+" "+nodes[getAllKeys[2]]["state"]+ " "+nodes[getAllKeys[2]]["label"]);
-			d3.select("body").select("#nodeFour").text(nodes[getAllKeys[3]]["name"] +" "+nodes[getAllKeys[3]]["state"]+ " "+nodes[getAllKeys[3]]["lable"]);
+			d3.select("body").select("#nodeFour").text(nodes[getAllKeys[3]]["name"] +" "+nodes[getAllKeys[3]]["state"]+ " "+nodes[getAllKeys[3]]["label"]);
 			d3.select("body").select("#nodeFive").text("state totals:" + stateTotals);
 		   d3.select("body").select("#nodeSix").text(getAllKeys);
             console.log(data);
+    
+    displayTasks(nodes, getAllKeys);
+
+    setStatesData(data);
 
 
-    //Get information from suite every interval.
-    last_updated = new Date(data[0].last_updated*1000);
-    runahead = data[0]['state totals'].runahead;
-    if(runahead == null | runahead == undefined){
-			runahead = "0";
-    }
-    waiting = data[0]['state totals'].waiting;
-    if(waiting == null | waiting == undefined){
-        waiting = "0";
-    }
-    succeeded = data[0]['state totals'].succeeded;
-    if(succeeded == null | succeeded == undefined){
-        succeeded = "0";
-    }
-    running = data[0]['state totals'].running;
-    if(running == null | running == undefined){
-        running = "0";
-    }
-
-    //Set element to the data from the JSON.
-    document.getElementById("information").innerHTML = last_updated;
-    document.getElementById("runahead").innerHTML = runahead;
-    document.getElementById("waiting").innerHTML = waiting;
-    document.getElementById("succeeded").innerHTML = succeeded;
-    document.getElementById("running").innerHTML = running;
 
 
-    //Output to the console. Error fixing.
+    /**
+     * Error Checking.
+     */ 
     console.log("State totals", data[0]['state totals']);
-    console.log("RUNAHEAD", data[0]["state totals"].runahead);
-    console.log("SUCCEEDED", data[0]["state totals"].succeeded);
-    console.log("OB2", data[1]['state']);
-    console.log("OBJ3 ", data[2]);
-
-
-    //Make all Elements Visible.
-    document.getElementById("information").style.visibility = "visible";
-    document.getElementById("table").style.visibility = "visible";
-    document.getElementById("runahead").style.visibility = "visible";
-    document.getElementById("updated").style.visibility = "visible";
-
-    //$('ul').html(buffer);
-
+    console.log("Obj0", data[0]);
+    console.log("Nodes", data[1]);
+    
     var th_elements = d3.select("body").select("table").select("tr").selectAll("th");
 
     for(var element in th_elements[0]) {
@@ -301,3 +268,109 @@ socket.on('summary_info',function(data){
             }
         }
 });
+
+/**
+ * Function: displayTasks
+ * Returns: void
+ * Summary: Puts labels(key) in a dictionary with a list(value) of tasks running for each label.
+ */
+function displayTasks(nodes, getAllKeys){
+
+    var list = [];
+    var cylc_tasks = {};
+
+    //console.log("KEYS", getAllKeys);  
+   // console.log("Length", getAllKeys.length);
+    
+    
+    for(var i = 0; i < getAllKeys.length; i++){ 
+        
+        var label = nodes[getAllKeys[i]]['label'];
+        var name = nodes[getAllKeys[i]]['name'];
+        var state = nodes[getAllKeys[i]]['state'];
+        
+        if(!(label in cylc_tasks)){
+            
+            //console.log("LABELin ", label);
+            //console.log("Namein ", name);
+
+            list.push(name);
+            list.push(state);
+            cylc_tasks[label] = list;
+            list = [];
+        }
+        else{
+            //console.log("Else");
+            cylc_tasks[label].push(name);
+            cylc_tasks[label].push(state);
+        }
+    }
+   
+  for(i in cylc_tasks){
+    console.log("Key is" + i);
+
+      console.log(typeof(cylc_tasks[i].length));
+
+      for(var j = 0; j < cylc_tasks[i].length; j++){
+        
+          //Even == STATE
+        if(j % 2 == 0){
+            console.log("State: ",cylc_tasks[i][j]);
+        }else{
+            console.log("Label:", cylc_tasks[i][j]);
+        }
+      }
+  }
+
+  console.log("LIST:", cylc_tasks);  
+}
+
+
+/**
+ * Function:setData
+ * Returns: void
+ * Summary: Checks if states are null (non-exsistent) and replaces them with a 0.
+ *          Sets the HTML tags with JSON info, and sets visible to true.
+ */
+
+function setStatesData(data){
+
+    last_updated = new Date(data[0].last_updated*1000);
+
+
+    runahead = data[0]['state totals'].runahead;
+    if(runahead == null | runahead == undefined){
+			runahead = "0";
+    }
+    waiting = data[0]['state totals'].waiting;
+    if(waiting == null | waiting == undefined){
+        waiting = "0";
+    }
+    succeeded = data[0]['state totals'].succeeded;
+    if(succeeded == null | succeeded == undefined){
+        succeeded = "0";
+    }
+    running = data[0]['state totals'].running;
+    if(running == null | running == undefined){
+        running = "0";
+    }
+
+
+    /**
+     * Set HTML tags with JSON data.
+     */
+    document.getElementById("runahead").innerHTML = runahead;
+    document.getElementById("waiting").innerHTML = waiting;
+    document.getElementById("succeeded").innerHTML = succeeded;
+    document.getElementById("running").innerHTML = running;
+    document.getElementById("information").innerHTML = last_updated;
+
+    /**
+     * Elements Visible
+     */
+    document.getElementById("information").style.visibility = "visible";
+    document.getElementById("table").style.visibility = "visible";
+    document.getElementById("runahead").style.visibility = "visible";
+    document.getElementById("updated").style.visibility = "visible"; 
+
+}
